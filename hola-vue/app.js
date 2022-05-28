ba06f90a73e465bb2b0c14fbeb4ffc0272595d03
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-05-28 18:45:27 +0200

    Clase 6

--- hola-vue/app.js
+++ hola-vue/app.js
@@ -1,8 +1,9 @@
 const vm = Vue.createApp({
   data() {
     return {
-      text: 'Hola vue',
+      text: '<p>Hola vue<p>',
     };
   },
+  template: `<div v-once v-html="text"></div>`,
 }).mount('#app');
 console.log(vm);
