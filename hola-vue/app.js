40611ed2b7ee4af6644a521aff1db656d148a946
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-05-29 18:27:04 +0200

    Clase 10

--- hola-vue/app.js
+++ hola-vue/app.js
@@ -1,14 +1,28 @@
 const vm = Vue.createApp({
   data() {
     return {
-      text: 'Hola vue',
+      firstName: 'Jose Ramon',
+      lastName: 'Vieites Romay',
+      now: new Date(),
     };
   },
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    today() {
+      return this.now.toLocaleDateString();
+    },
+  },
+  methods: {
+    fullName1() {
+      return this.firstName + ' ' + this.lastName;
+    },
+  },
   template: `
-  <p>{{text}}</p>
-  <input 
-    type="text" 
-    v-model="text" />
+  <div>{{ fullName1() }}</div>
+  <div>{{ fullName }}</div>
+  <div>{{ today }}</div>
   `,
 }).mount('#app');
 console.log(vm);
