e35bbbdd77c1b27cae389e19f7fcd2aac9dc0309
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-06-03 19:56:51 +0200

    Clase 22

---
+++ Hola.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data(){
+        return {
+            text: "Hola"
+        }
+    }
+}
+</script>
+
+<template>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, a. Laborum ex blanditiis doloremque, molestias repellat suscipit debitis inventore voluptas sit, iusto, sint incidunt amet facilis? Repellat eos deleniti saepe!</p>
+</template>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
